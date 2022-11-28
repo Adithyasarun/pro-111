6fe950a992b003e892494a0e07509e459cf8559b
Adithyasarun <92289063+Adithyasarun@users.noreply.github.com>
2022-11-28 18:52:06 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+import random
+no = random.randint(1,6)
+if no == 1 :
+    print("0")
+if no == 2 :
+    print("1")
+if no == 3 :
+    print("2")
+if no == 4 :
+    print("3")
+if no == 5 :
+    print("4")
+if no == 6 :
+    print("5")
+
